e866a004b0ed95c7cc021ef26416451ee2106f08
JocelynVelarde <a01285780@itesm.mx>
2024-05-13 13:15:24 -0600

    graph eda and bvp values and allows csv download of datasets

--- home.py
+++ home.py
@@ -1,28 +1,33 @@
 import streamlit as st
 
-st.title("Get Started with our platform! 💡")
+st.set_page_config(
+        page_title="EmbracePlus Visualizer",
+        page_icon="📈",
+)
+st.title("Get Started with EmbracePlus Visualizer 📈")
 
 st.divider()
 
-st.header(":blue[Tell us your location and destination to get the safest route]")
-st.write("1. Type your desired route in the search bar.")
-st.write("2. Speak your desired route using the microphone button.")
-st.write("3. Click on the 'Search Route' button to get the safest route to your destination.")
-st.write("4. View your route on an interactive map")
-st.write("5. Also get understandable instructions to reach your destination.")
+st.header(":blue[Upload your .avro files and we will graph them for you!]")
+st.write("1. Click on the 'Browse files' button to upload your AVRO files. You can select all of the files related to a session at once.")
+st.write("2. Once you have uploaded the files, click on the 'Visualize' button to see the graphs.")
+st.write("3. You can also download the data in CSV format by clicking on the 'Download CSV' button.")
+st.write("4. If you want to clear the uploaded files, click on the 'Clear Files' button.")
+st.write("5. If you want to save the graphs as an image, click on the 'Save as Image' button.")
 
-st.header("Example of a user's request")
+
+st.header("Example of a processed graph:")
 
 container = st.container(border=True)
 
-st.caption("You can just go ahead and type or speak any way you like and we will understand you.")
-container.write("I want to go from Churubusco to Coapa but I do not know how, could you help me?")
+st.caption("Note: This is an example of a graph that will be generated after uploading the files. The actual graph will be based on the data in your files.")
+container.image("assets/example.png", use_column_width=True)
 
 
 st.divider()
 
-st.page_link("pages/visualize.py", label="Click this button to Search Route", icon="🔍")
+st.page_link("pages/visualize.py", label="Click this button to start uploading", icon="📁")
 
 st.divider()
 
-st.write("Thank you for choosing SafeNav!")
+st.write("Code by: [JocelynVelarde](https://github.com/JocelynVelarde)")
--- pages/visualize.py
+++ pages/visualize.py
@@ -1,19 +1,14 @@
 import pandas as pd
 import fastavro
 import json
-import csv
-import json
-import ast
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-
-
 def convert_files(uploaded_files):
     dataframes = []
     combined_eda = pd.DataFrame()
+    combined_bvp = pd.DataFrame()
 
     for file in uploaded_files:
         reader = fastavro.reader(file)
@@ -21,46 +16,91 @@ def convert_files(uploaded_files):
         df = pd.DataFrame.from_records(records)
         dataframes.append(df)
         if df['rawData'].apply(lambda x: isinstance(x, dict)).all():
-            # If all elements in 'rawData' are dictionaries, no need to use json.loads
             eda = df['rawData'].apply(lambda x: x['eda']).apply(pd.Series)
+            bvp = df['rawData'].apply(lambda x: x['bvp']).apply(pd.Series)
         else:
-            # If not all elements in 'rawData' are dictionaries, use json.loads
             eda = df['rawData'].apply(json.loads).apply(lambda x: x['eda']).apply(pd.Series)
+            bvp = df['rawData'].apply(json.loads).apply(lambda x: x['bvp']).apply(pd.Series)
         combined_eda = combined_eda.append(eda, ignore_index=True)
+        combined_bvp = combined_bvp.append(bvp, ignore_index=True)
 
       
     combined_eda = combined_eda.sort_values('timestampStart')
     combined_eda = combined_eda.explode('values')
-    time_data = []
+    combined_bvp = combined_bvp.sort_values('timestampStart')
+    combined_bvp = combined_bvp.explode('values')
+    
+    time_data_eda = []
+    time_data_bvp = []
     eda_data = []
-    accumulated_time = 0 
+    bvp_data = []
+    accumulated_time_eda = 0 
+    accumulated_time_bvp = 0
 
     for _, row in combined_eda.iterrows():
         values = row['values']
         time = np.arange(0, 1/row['samplingFrequency'], 1/row['samplingFrequency'])
-        time += accumulated_time
-        accumulated_time = time[-1]
-        #time_data = [t / row['samplingFrequency'] for t in time_data]
-        time_data.extend(time)
+        time += accumulated_time_eda
+        accumulated_time_eda = time[-1]
+        time_data_eda.extend(time)
         eda_data.append(values)
 
-    data = pd.DataFrame({
-        'Time': time_data,
+    for _, row in combined_bvp.iterrows():
+        values = row['values']
+        time = np.arange(0, 1/row['samplingFrequency'], 1/row['samplingFrequency'])
+        time += accumulated_time_bvp
+        accumulated_time_bvp = time[-1]
+        time_data_bvp.extend(time)
+        bvp_data.append(values)
+
+    data_eda = pd.DataFrame({
+        'Time': time_data_eda,
         'EDA': eda_data
     })
 
-    st.line_chart(data)
-    st.write(combined_eda)
-    
+    data_bvp = pd.DataFrame({
+        'Time': time_data_bvp,
+        'BVP': bvp_data
+    })
 
+    st.title("EDA Data")
+    st.divider()
+    st.line_chart(data_eda)
+
+    st.title("BVP Data")
+    st.divider()
+    st.line_chart(data_bvp)
+
+    st.title("Dataframe download")
+    st.write("EDA Data")
+    st.write(combined_eda)
+    st.divider()
+    st.write("BVP Data")
+    st.write(combined_bvp)
+    
     return dataframes
 
 
 import streamlit as st
 
-st.write("Upload your AVRO files here and we will graph them for you!")
+st.set_page_config(
+        page_title="EmbracePlus Visualizer",
+        page_icon="📈",
+)
+
+st.title("Upload your .avro files here to visualize them 🔍")
 
 uploaded_files = st.file_uploader("Upload Files", type="avro", accept_multiple_files=True)
-dataframes = convert_files(uploaded_files)
+visualize_files = st.button("Upload Files")
+
+if visualize_files and uploaded_files:
+    st.success("Files uploaded successfully, visualizing...")
+    dataframes = convert_files(uploaded_files)
+
+else:
+    st.warning("Please upload files to visualize")
 
 
+st.divider()
+
+st.write("Code by: [JocelynVelarde](https://github.com/JocelynVelarde)")
